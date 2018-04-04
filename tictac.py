6d962244302f7ae110f6abce689daab8ecea167b
Untitled <john.d.reddock@gmail.com>
2018-04-04 13:07:56 -0400

    added player_choice and place_marker functions

--- tictac.py
+++ tictac.py
@@ -27,6 +27,7 @@ def player_input():
 
 # takes board list object, a marker (X or O) and a position (1-9) and assigns to the board
 def place_marker(board, marker, position):
+    board[position] = marker
     pass
 
 # takes in a board and a mark (X or O) and checks to see if that mark has won
@@ -35,11 +36,10 @@ def win_check(board, mark):
     (board[4] == board[5] == board[6]) or #across center
     (board[1] == board[2] == board[3]) or #across bottom
     (board[7] == board[4] == board[1]) or #down left side
-    (board[8] == board[5] == board[1]) or #down center
+    (board[8] == board[5] == board[2]) or #down center
     (board[9] == board[6] == board[3]) or #down right side
     (board[7] == board[5] == board[3]) or #diag
-    (board[9] == board[5] == board[1])) or #other diag
-
+    (board[9] == board[5] == board[1])) #other diag
     pass
 
 # randomly decide who goes first
@@ -61,6 +61,11 @@ def full_board_check(board):
 # asks for player's next position (1-9) and then uses space_check if it's free.
 # if it's a free position, return the position for later use
 def player_choice(board):
+    move = ''
+    while move not in '1 2 3 4 5 6 7 8 9'.split() or not space_check(board, int(move)):
+        print ('what is your move? (1-9)')
+        move = input()
+        return int(move)
     pass
 
 # does player want to play again? returns boolean
@@ -75,15 +80,23 @@ def replay():
 print('Welcome to Tic Tac Toe')
 
 while True:
+    board = [' '] * 10
+    player1, player2 = player_input()
+    turn = choose_first()
+    print (turn + ' goes first')
     game_on = True
     while game_on:
-        # display_board(board)
-        player1, player2 = player_input()
+        display_board(board)
+        
         #player 1 turn
-
+        if turn == 'player1':
+            mark = player_choice(board)
+            place_marker(board, player1, mark)            
+            
         #player 2 turn
-            #pass
-
+        if turn == 'player2':
+            mark = player_choice(board)
+            place_marker(board, player2, mark)
     if not replay():
         break 
  
